95a08f4fed1a3d0f4e4febb30fb6b59a0aca6e08
Hana Ben Hadj Abdallah <h.benhadj@root.sa>
2018-03-13 16:46:35 +0100

    Add project files.

---
+++ Search/Search/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Search
+{
+	public partial class MainPage : ContentPage
+	{
+        List<ShopList> L;
+
+        public MainPage()
+		{
+			InitializeComponent();
+            L = new List<ShopList>
+            {
+               new ShopList { Title = "Gucci Hand Bag", Type="Almost new", Price ="30000", Image ="sac11.png" },
+               new ShopList {Title ="Mango Hand Bag", Type="Almost new", Price ="30000" ,Image ="sac22.png" },
+               new ShopList {Title ="Guess Hand Bag", Type="Almost new", Price ="30000",Image ="sac33.png"},
+            };
+            ListViewSearch.ItemsSource = L;
+
+
+        }
+       
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //thats all you need to make a search 
+            
+            if (string.IsNullOrEmpty(e.NewTextValue))
+            {
+                ListViewSearch.ItemsSource = L;
+            }
+
+            else
+            {
+                ListViewSearch.ItemsSource = L.Where(x => x.Title.ToLower().StartsWith(e.NewTextValue.ToLower()));
+            }
+        }
+    }
+}
---
+++ Search/Search/ShopList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Search
+{
+    class ShopList
+    {
+        public String Title { get; set; }
+        public String Type { get; set; }
+        public String Price { get; set; }
+        public String Image { get; set; }
+
+
+    }
+}
